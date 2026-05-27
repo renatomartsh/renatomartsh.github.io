d4ad96f3adb3907685b147e7b349d2c72813a4a7
Renato <renatomartsh@gmail.com>
2026-05-27 19:03:13 -0300

    add login

---
+++ js/auth.js
@@ -0,0 +1,27 @@
+async function login() {
+
+    const user = document.getElementById("user").value;
+    const pass = document.getElementById("pass").value;
+
+    const response = await fetch("data/users.json");
+    const users = await response.json();
+
+    const found = users.find(u =>
+        u.user === user &&
+        u.password === pass
+    );
+
+    if(found){
+
+        // cria "sessão"
+        sessionStorage.setItem("auth", "true");
+
+        // salva horário login
+        sessionStorage.setItem("loginTime", Date.now());
+
+        window.location.href = "dashboard.html";
+
+    } else {
+        alert("Login inválido");
+    }
+}
